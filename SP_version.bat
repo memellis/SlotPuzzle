7e3f684597b13c000d3dd01e3a364a51ca4562e7
Mark Ellis <memellis@gmail.com>
2011-12-30 15:57:36 +0000

    Refined versioning with source/Version.h containing version #define's

--- SP_version.bat
+++ SP_version.bat
@@ -1,10 +1,10 @@
-et OS_GIT_REPO=c:\OpenSim_GIT
+@echo off
+set OS_GIT_REPO="c:\Users\Mark Ellis\Documents\My Develop\SlotPuzzle"
 :: ================================
 :: Appending Path to access all binaries used by GIT
 ::
 PATH = C:\Program Files (x86)\Git\bin;%PATH%; 
-echo %PATH%
 ::
 cd %OS_GIT_REPO%
 ::
-sh.exe revtag.sh
+sh.exe -x revtag.sh
--- revtag.sh
+++ revtag.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+echo "/*" > source/Version.h
+echo " * Version.h" >> source/Version.h
+echo " * Created on: "`date` >> source/Version.h
+echo " *     Author: Mark Ellis" >> source/Version.h
+echo " *" >> source/Version.h
+echo "*/" >> source/Version.h
+echo "" >> source/Version.h
+echo "#ifndef VERSION_H_" >> source/Version.h
+echo "define VERSION_H_" >> source/Version.h
+echo "" >> source/Version.h
+echo "#define SLOTPUZZLE_HASH_COMMIT \""`git log -n 1 --pretty="format:%h"`"\"" >>source/Version.h 
+echo "#define SLOTPUZZLE_HASH_COMMIT_TIME \""`git log -n 1 --pretty="format:%ci"`"\"" >>source/Version.h 
+echo "#define SLOTPUZZLE_BUILD \""`git rev-list --all | wc -l | sed "s/\[ \t]//g"`"\"" >>source/Version.h
+echo "" >>source/Version.h
+echo "#endif /* VERSION_H_ */" >> source//Version.h
 
-echo "#define SLOTPUZZLE_HASH_COMMIT \""`git log -n 1 --pretty="format:%h"`"\"" >source/version.h 
-echo "#define SLOTPUZZLE_HASH_COMMIT_TIME \""`git log -n 1 --pretty="format:%ci"`"\"" >>source/version.h 
-echo "#define SLOTPUZZLE_BUILD \""`git rev-list --all | wc -l | sed "s/\[ \t]//g"`"\"" >>source/version.h
