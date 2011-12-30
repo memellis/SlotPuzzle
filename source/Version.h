883a73c48a42678b0fd9ede96fc36c46209ea972
Mark Ellis <memellis@gmail.com>
2011-12-30 16:50:53 +0000

    More testing of version numbering

--- source/Version.h
+++ source/Version.h
@@ -1,6 +1,6 @@
 /*
  * Version.h
- * Created on: Fri Dec 30 16:04:33 GMTST 2011
+ * Created on: Fri Dec 30 16:49:33 GMTST 2011
  *     Author: Mark Ellis
  *
 */
@@ -8,8 +8,8 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define SLOTPUZZLE_HASH_COMMIT 0x254df56
-#define SLOTPUZZLE_HASH_COMMIT_TIME "2011-12-30 16:00:27 +0000"
-#define SLOTPUZZLE_BUILD_NUMBER 21
+#define SLOTPUZZLE_HASH_COMMIT 0x3b1ae44
+#define SLOTPUZZLE_HASH_COMMIT_TIME "2011-12-30 16:41:19 +0000"
+#define SLOTPUZZLE_BUILD_NUMBER  22
 
 #endif /* VERSION_H_ */
