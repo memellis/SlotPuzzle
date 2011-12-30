70cdc5d64c999e6af734b178f150accb2aef89d6
Mark Ellis <memellis@gmail.com>
2011-12-30 15:20:41 +0000

    Refined version numbering

--- revtag.sh
+++ revtag.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 echo "#define APP_VERSION \""`git log -n 1 --pretty="format:%h %ci"`"\"" >source/version.h 
+echo "#define SLOTPUZZLE_BUILD \""`git rev-list --all | wc -l | sed "s/\[ \t]//g"`"\"" >>source/version.h
