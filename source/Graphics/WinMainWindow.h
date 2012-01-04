b25b5e530d4736ceaf1df6a0595f965b04e3b199
Mark Ellis <memellis@gmail.com>
2012-01-04 16:32:54 +0000

    Main window display a Direct3D rendered triangle forward enginneering from BoUML

--- source/Graphics/WinMainWindow.h
+++ source/Graphics/WinMainWindow.h
@@ -4,28 +4,30 @@
 
 #include <windows.h>
 #include <d3dx9.h>
-
+#include "../Resources/resource.h"
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 
 //error macro for Error Message dialog box
 #define ERROR_MESSAGE(msg){MessageBox(NULL,msg,L"Error",MB_OK|MB_ICONEXCLAMATION);}
-
 class WinMainWindow {
   public:
     int version;
 
-	IDirect3D9 *m_pD3D;
-	IDirect3DDevice9 *m_pD3DDevice;
+ 
+ 
+ 
+    HRESULT initialise();
 
-	IDirect3DVertexBuffer9 *m_pVertexBuffer;
+    HRESULT render();
 
-	HRESULT initialise();
+    void cleanUp();
 
-	HRESULT render();
+    IDirect3D9 *m_pD3D;
 
-	void cleanUp();
+    IDirect3DDevice9 *m_pD3DDevice;
+
+    IDirect3DVertexBuffer9 *m_pVertexBuffer;
+
 };
-
-
 #endif
