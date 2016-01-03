57e5c8d93975629482282c2aa3d1f96613471e23
mellis0 <memellis@gmail.com>
2016-01-03 12:10:02 +0000

    Added Andriod target

---
+++ source/java/2d/desktop/src/com/ellzone/slotpuzzle2d/desktop/MyPacker.java
@@ -0,0 +1,10 @@
+package com.ellzone.slotpuzzle2d.desktop;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+public class MyPacker {
+    public static void main (String[] args) throws Exception {
+        TexturePacker.process("reel", "reel", "reels.pack");
+    }
+}
+
