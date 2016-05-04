7dd0e4de78e2e0ad48e837448f8fc3005ecbb0f2
Mark Ellis <memellis@gmail.com>
2016-05-04 22:30:26 +0100

    Test ReelLetter.java some bugs fixed but one remains stopping slot spinning.

--- source/java/2d/tests/src/com/ellzone/TestReelLetterSprite.java
+++ source/java/2d/tests/src/com/ellzone/TestReelLetterSprite.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.Array;
 import com.ellzone.slotpuzzle2d.SlotPuzzle;
 import com.ellzone.slotpuzzle2d.screens.IntroScreen;
@@ -26,10 +27,11 @@ public class TestReelLetterSprite {
 
 	private static final String SLOT_PUZZLE_REEL_TEXT = "Slot Puzzle";
 	private static final int EXO_FONT_SMALL_SIZE = 24;
+	private static final int REEL_SIZE_WIDTH = 40;
+	private static final int REEL_SIZE_HEIGHT = 40;
 	private static final int SCROLL_HEIGHT = 20;
 	private static final int SCROLL_STEP = 4;
 	private static final float SIXTY_FPS = 1.0f / 60.0f;
-	private static final int TEXT_SPACING_SIZE = 30;
 	private static final String BY_TEXT = "by";
 	private SmartFontGenerator fontGen;
 	private BitmapFont fontSmall;
@@ -60,11 +62,16 @@ public class TestReelLetterSprite {
 	
 	@Test
 	public void TestReelLetterSpriteCreation() {
-		Pixmap textPixmap = new Pixmap(TestReelLetterSprite.EXO_FONT_SMALL_SIZE, TestReelLetterSprite.SLOT_PUZZLE_REEL_TEXT.length() * TestReelLetterSprite.SCROLL_HEIGHT, Pixmap.Format.RGBA8888);
-		textPixmap = PixmapProcessors.createDynamicVerticalFontText(fontSmall, TestReelLetterSprite.SLOT_PUZZLE_REEL_TEXT, textPixmap);
-		textPixmap = PixmapProcessors.createDynamicScrollAnimatedVerticalText(textPixmap, TestReelLetterSprite.SCROLL_HEIGHT, TestReelLetterSprite.SLOT_PUZZLE_REEL_TEXT, TestReelLetterSprite.EXO_FONT_SMALL_SIZE, TestReelLetterSprite.SCROLL_STEP);
+		Pixmap textPixmap = new Pixmap(TestReelLetterSprite.REEL_SIZE_WIDTH, TestReelLetterSprite.BY_TEXT.length() * TestReelLetterSprite.REEL_SIZE_HEIGHT, Pixmap.Format.RGBA8888);
+		textPixmap = PixmapProcessors.createDynamicVerticalFontText(fontSmall, TestReelLetterSprite.BY_TEXT, textPixmap);
+		FileHandle pixmapFile = Gdx.files.local("reelLetterSpriteVertical.png");
+		if (pixmapFile.exists()) {
+			pixmapFile.delete();
+		}
+		PixmapProcessors.savePixmap(textPixmap, pixmapFile.file());
+		textPixmap = PixmapProcessors.createDynamicScrollAnimatedVerticalText(textPixmap, TestReelLetterSprite.SCROLL_HEIGHT, TestReelLetterSprite.BY_TEXT, TestReelLetterSprite.EXO_FONT_SMALL_SIZE, TestReelLetterSprite.SCROLL_STEP);
 		Texture textTexture = new Texture(textPixmap);
-		FileHandle pixmapFile = Gdx.files.local("reelLetterSprite.png");
+		pixmapFile = Gdx.files.local("reelLetterSpriteScrolling.png");
 		if (pixmapFile.exists()) {
 			pixmapFile.delete();
 		}
@@ -73,19 +80,25 @@ public class TestReelLetterSprite {
 		assertTrue(pixmapFile.exists());
 		Screen screen = null;
 		Array<ReelLetter> reelLetters = new Array<ReelLetter>();
-		for (int i = 0; i < TestReelLetterSprite.BY_TEXT.length(); i++) {
-			reelLetters.add(new ReelLetter(screen, textTexture, TestReelLetterSprite.SLOT_PUZZLE_REEL_TEXT.length(), TestReelLetterSprite.BY_TEXT.length() * 5 - 1, TestReelLetterSprite.SIXTY_FPS, (i * TestReelLetterSprite.TEXT_SPACING_SIZE) + 800 / 3.2f, 480 / 2.0f + TestReelLetterSprite.TEXT_SPACING_SIZE + 10, i));
+		for (int i = 0; i < TestReelLetterSprite.BY_TEXT.length(); i++) {			
+			reelLetters.add(new ReelLetter(screen, textTexture, TestReelLetterSprite.BY_TEXT.length(), textPixmap.getWidth() / TestReelLetterSprite.REEL_SIZE_WIDTH, TestReelLetterSprite.SIXTY_FPS, (i * TestReelLetterSprite.REEL_SIZE_WIDTH) + 800 / 3.2f, 480 / 2.0f + TestReelLetterSprite.REEL_SIZE_HEIGHT + 10, i));
 		}
 
 		Array<Pixmap> startPixmaps = new Array<Pixmap>();
-		for (ReelLetter reel : reelLetters) {
-			startPixmaps.add(PixmapProcessors.getPixmapFromSprite(reel));
+		
+		for (int letter = 0; letter < TestReelLetterSprite.BY_TEXT.length(); letter++) {
+			TextureRegion startReelLetter = new TextureRegion(textTexture, 0, letter * TestReelLetterSprite.REEL_SIZE_HEIGHT, TestReelLetterSprite.REEL_SIZE_WIDTH, TestReelLetterSprite.REEL_SIZE_HEIGHT);
+			startPixmaps.add(PixmapProcessors.getPixmapFromtextureRegion(startReelLetter));
+			FileHandle reelLetterPixmapFile = Gdx.files.local("startReelLetterPixmap" + letter + ".png");
+			if (reelLetterPixmapFile.exists()) {
+				reelLetterPixmapFile.delete();
+			}
+			PixmapProcessors.savePixmap(startPixmaps.get(letter), reelLetterPixmapFile.file());			
 		}
 		
 		float dt = 0.0f;
-		float inc = 1.0f / 60.0f; 
 		int spinCount = TestReelLetterSprite.BY_TEXT.length();
-		while ((dt < 1) & (spinCount != 0)) { 
+		while ((dt < 10) & (spinCount > 0)) { 
 			for(ReelLetter reel : reelLetters) {
 				reel.update(dt);
 				if (!reel.isSpinning()) {
@@ -95,22 +108,21 @@ public class TestReelLetterSprite {
 			dt = dt + TestReelLetterSprite.SIXTY_FPS;
 		}
 
-		Array<FileHandle> fileHandles = new Array<FileHandle>();
 		int endReel = 0;
+		Pixmap endPixmap;
 		for(ReelLetter reel : reelLetters) {
 			FileHandle reelLetterPixmapFile = Gdx.files.local("reelLetterPixmap" + endReel + ".png");
 			if (reelLetterPixmapFile.exists()) {
 				reelLetterPixmapFile.delete();
-			}
-			System.out.println("reel.getRegionX="+reel.getRegionX());
-			System.out.println("reel.getRegionY="+reel.getRegionY());
-			System.out.println("reel.getRegionWidth="+reel.getRegionWidth());
-			System.out.println("reel.getRegionHeight="+reel.getRegionHeight());
+			}		
+			endPixmap = PixmapProcessors.getPixmapFromSprite(reel);
+			endPixmap = PixmapProcessors.scrollPixmapWrap(endPixmap, TestReelLetterSprite.SCROLL_STEP - 1);
+			PixmapProcessors.savePixmap(endPixmap, reelLetterPixmapFile.file());
 
-			PixmapProcessors.savePixmap(PixmapProcessors.getPixmapFromSprite(reel), reelLetterPixmapFile.file());
 			assertTrue(endReel == reel.getEndReel());
-			//assertTrue(PixmapProcessors.arePixmapsEqual(startPixmaps.get(endReel), PixmapProcessors.getPixmapFromSprite(reel)));
+			assertTrue(PixmapProcessors.arePixmapsEqual(startPixmaps.get(endReel), endPixmap));
 			endReel++;
 		}
+		
 	}
 }
