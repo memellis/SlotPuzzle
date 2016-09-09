95c54d8a165ee8166dc54e6974ec8dfe7d3e1a7a
mellis0 <memellis@gmail.com>
2016-09-09 20:12:32 +0100

    Added GameOverPopUp class

--- source/java/2d/desktop/src/com/ellzone/slotpuzzle2d/desktop/MyPacker.java
+++ source/java/2d/desktop/src/com/ellzone/slotpuzzle2d/desktop/MyPacker.java
@@ -3,7 +3,7 @@ package com.ellzone.slotpuzzle2d.desktop;
 import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 public class MyPacker {
     public static void main (String[] args) throws Exception {
-        TexturePacker.process("reel", "reel", "reels.pack");
+        TexturePacker.process("../android/assets/tiles", "../android/assets/tiles", "tiles.pack");
     }
 }
 
