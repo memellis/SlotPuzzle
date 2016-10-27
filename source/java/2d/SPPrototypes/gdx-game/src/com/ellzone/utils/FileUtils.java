5e50c6b83ea3bd34261cef4424f1b616587042bf
Mark Ellis <memellis@gmail.com>
2016-10-27 08:52:47 +0100

    SPPrototypes working in Eclipse IDE

--- source/java/2d/SPPrototypes/gdx-game/src/com/ellzone/utils/FileUtils.java
+++ source/java/2d/SPPrototypes/gdx-game/src/com/ellzone/utils/FileUtils.java
@@ -6,27 +6,40 @@ import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.Gdx;
 
 public class FileUtils {
+	FileHandle[] files;
+	Map<String, FileHandle> fileHandles;
+	
 	public static final String LOG_TAG = "FileUtils";
 	
 	public FileUtils() {
 	}
 	
-	public static void gatherJavaFiles (FileHandle dir, Set<String> names, Map<String, FileHandle> fileHandles, boolean recursive) {
-		FileHandle[] files = dir.list();
+	public void gatherJavaFiles (FileHandle dir, Set<String> names, Map<String, FileHandle> fileHandles, boolean recursive) {
+		this.fileHandles = fileHandles;
+		files = dir.list();
 		for (FileHandle file : files) {
 			if (file.isDirectory() && recursive) {
+				Gdx.app.log(LOG_TAG, "Found directory " + file.name());
 				gatherJavaFiles(file, names, fileHandles, recursive);
 			} else {
-				if (file.extension().equals("java")) {
+				if ((file.extension().equals("java") |
+				    (file.extension().equals("class")))) {
 					Gdx.app.log(LOG_TAG, "Found java file " + file.name());
 					if (names.contains(file.name())) {
-						System.out.println(file.name() + " duplicate!");
+						Gdx.app.log(LOG_TAG, file.name() + " duplicate!");
 					}
 					names.add(file.name());
 					fileHandles.put(file.name(), file);
 				}
 			}
 		}
+	}
+	
+	public int getNumJavaFilesFound() {
+		if (this.fileHandles == null) {
+			return 0;
+		}
+		return this.fileHandles.size();
 	}
 }
 
