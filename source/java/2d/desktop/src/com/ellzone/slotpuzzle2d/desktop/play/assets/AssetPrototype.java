ddfbfac519734f7a0bca8a88c2c5d202311ee6af
mellis0 <memellis@gmail.com>
2016-12-24 22:01:56 +0000

    Added to repository

---
+++ source/java/2d/desktop/src/com/ellzone/slotpuzzle2d/desktop/play/assets/AssetPrototype.java
@@ -0,0 +1,7 @@
+package com.ellzone.slotpuzzle2d.desktop.play.assets;
+
+import com.ellzone.slotpuzzle2d.desktop.play.PlayPrototype;
+
+public abstract class AssetPrototype extends PlayPrototype {
+
+}
