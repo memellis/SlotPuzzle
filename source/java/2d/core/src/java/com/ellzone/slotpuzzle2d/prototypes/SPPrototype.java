79610492b10c95b5e47904d5f0daac662ed95b7b
mellis0 <memellis@gmail.com>
2017-03-12 01:02:36 +0000

    Added to repository

---
+++ source/java/2d/core/src/java/com/ellzone/slotpuzzle2d/prototypes/SPPrototype.java
@@ -0,0 +1,25 @@
+package com.ellzone.slotpuzzle2d.prototypes;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.InputAdapter;
+
+public abstract class SPPrototype extends InputAdapter implements ApplicationListener {
+
+    public void create () {
+    }
+
+    public void resume () {
+    }
+
+    public void render () {
+    }
+
+    public void resize (int width, int height) {
+    }
+
+    public void pause () {
+    }
+
+    public void dispose () {
+    }
+}
