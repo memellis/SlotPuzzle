851e8fe10db304475b133636f99f2a35c388b830
mellis0 <memellis@gmail.com>
2017-05-31 19:35:20 +0100

    Adjusting for merge errors and move to libGDX version 1.9.6

--- source/java/2d/core/src/java/com/ellzone/slotpuzzle2d/prototypes/SPPrototypeTemplate.java
+++ source/java/2d/core/src/java/com/ellzone/slotpuzzle2d/prototypes/SPPrototypeTemplate.java
@@ -25,7 +25,6 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.MathUtils;
-import com.ellzone.slotpuzzle2d.effects.SpriteAccessor;
 import com.ellzone.slotpuzzle2d.tweenengine.SlotPuzzleTween;
 import com.ellzone.slotpuzzle2d.tweenengine.TweenManager;
 import com.ellzone.slotpuzzle2d.utils.Assets;
