69ef5757ceacf515e1f8bd63f7c6a255d39ae417
root <root@localhost>
2017-06-08 21:22:33 +0000

    Added prototypes

---
+++ source/java/2d/SlotPuzzleAideTemplate/slotpuzzle-prototypes-android/src/com/ellzone/spprototypes/android/AndroidLauncher.java
@@ -0,0 +1,15 @@
+package com.ellzone.spprototypes.android;
+
+import android.os.Bundle;
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.ellzone.slotpuzzle2d.SlotPuzzle;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SlotPuzzle(), config);
+	}
+}
