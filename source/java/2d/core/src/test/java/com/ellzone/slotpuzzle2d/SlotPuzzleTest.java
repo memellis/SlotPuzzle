371dc7408c5fab8c62d2c4a9b7f1571f18a954f5
mellis0 <memellis@gmail.com>
2017-09-17 19:05:53 +0100

    Support Mockito based tests

--- source/java/2d/core/src/test/java/com/ellzone/slotpuzzle2d/SlotPuzzleTest.java
+++ source/java/2d/core/src/test/java/com/ellzone/slotpuzzle2d/SlotPuzzleTest.java
@@ -1,18 +1,32 @@
 package com.ellzone.slotpuzzle2d;
 
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.ellzone.slotpuzzle2d.screens.WorldScreen;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
-import org.mockito.Mockito;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
 
-public class SlotPuzzleTest extends GameTest {
+@RunWith(MockitoJUnitRunner.class)
+public class SlotPuzzleTest {
+
+    @Before
+    public void mockGameMethods() {
+    }
+
     @Test
     public void testSlotPuzzle() {
-        SlotPuzzle slotPuzzle = new SlotPuzzle();
-        Mockito.mock(SpriteBatch.class);
-        slotPuzzle.create();
-        assertNotNull(slotPuzzle);
+
     }
 }
