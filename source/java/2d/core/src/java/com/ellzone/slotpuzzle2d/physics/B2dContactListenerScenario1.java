3a93d91c48caf29fbcf7a823f7389ec66c99a9fa
mellis0 <memellis@gmail.com>
2018-04-11 21:36:31 +0100

    MiniSlotMachineLevelPrototypeScenario1 basic working. version.

--- source/java/2d/core/src/java/com/ellzone/slotpuzzle2d/physics/B2dContactListenerScenario1.java
+++ source/java/2d/core/src/java/com/ellzone/slotpuzzle2d/physics/B2dContactListenerScenario1.java
@@ -41,11 +41,9 @@ public class B2dContactListenerScenario1 implements ContactListener {
             classB = bodyB.getUserData().getClass().getName();
         }
         if(classA.equalsIgnoreCase("com.ellzone.slotpuzzle2d.prototypes.minislotmachine.MiniSlotMachineLevelPrototypeScenario1") && classB.equalsIgnoreCase("com.ellzone.slotpuzzle2d.sprites.ReelTile")) {
-            System.out.println("dealWithReelsHittingSinkBottom beginContact classA = " + classA + " classB=" + classB);
             dealWithReelsHittingSinkBottom((MiniSlotMachineLevelPrototypeScenario1) bodyA.getUserData(), (ReelTile) bodyB.getUserData());
         }
         if(classA.equalsIgnoreCase("com.ellzone.slotpuzzle2d.sprites.ReelTile") && classB.equalsIgnoreCase("com.ellzone.slotpuzzle2d.prototypes.minislotmachine.MiniSlotMachineLevelPrototypeScenario1")) {
-            System.out.println("dealWithReelsHittingSinkBottom beginContact classA = " + classA + " classB=" + classB);
             dealWithReelsHittingSinkBottom((MiniSlotMachineLevelPrototypeScenario1) bodyB.getUserData(), (ReelTile) bodyA.getUserData());
         }
         if((classA.equalsIgnoreCase("com.ellzone.slotpuzzle2d.sprites.ReelTile") && (classB.equalsIgnoreCase("com.ellzone.slotpuzzle2d.sprites.ReelTile")))) {
@@ -59,28 +57,21 @@ public class B2dContactListenerScenario1 implements ContactListener {
 
     @Override
     public void preSolve(Contact contact, Manifold oldManifold) {
-
     }
 
     @Override
     public void postSolve(Contact contact, ContactImpulse impulse) {
-
     }
 
     private void dealWithReelsHittingSinkBottom(MiniSlotMachineLevelPrototypeScenario1 prototype, ReelTile reelTile) {
         MiniSlotMachineLevelPrototypeScenario1.numberOfReelsToHitSinkBottom++;
         if (MiniSlotMachineLevelPrototypeScenario1.numberOfReelsToHitSinkBottom >= MiniSlotMachineLevelPrototypeScenario1.MAX_NUMBER_OF_REELS_HIT_SINK_BOTTOM) {
-            System.out.println("Reels Tiles have fallen to the bottom of the sink!");
             prototype.dealWithHitSinkBottom(reelTile);
             this.prototype = prototype;
         }
     }
 
     private void dealWithReelTileHittingReelTile(ReelTile reelTileA, ReelTile reelTileB) {
-        System.out.println("in dealWithReelTileHittingReelTile...");
-        System.out.println("reelTileA="+reelTileA+" reelTileA.x="+reelTileA.getX()+" reelTileA.y="+reelTileA.getY());
-        System.out.println("reelTileB="+reelTileB+" reelTileB.x="+reelTileB.getX()+" reelTileB.y="+reelTileB.getY());
-
         MiniSlotMachineLevelPrototypeScenario1.numberOfReelsToFall--;
         if (MiniSlotMachineLevelPrototypeScenario1.numberOfReelsToFall <= 0) {
             if (this.prototype != null) {
@@ -90,5 +81,4 @@ public class B2dContactListenerScenario1 implements ContactListener {
             }
         }
     }
-
 }
