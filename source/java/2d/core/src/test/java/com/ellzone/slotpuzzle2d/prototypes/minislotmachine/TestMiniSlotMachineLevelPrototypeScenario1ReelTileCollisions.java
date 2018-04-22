15601445003c6b73cfeaac221d8566e1357a5fd3
mellis0 <memellis@gmail.com>
2018-04-22 01:01:58 +0100

    Two working tests; Clean code - extracted methods

--- source/java/2d/core/src/test/java/com/ellzone/slotpuzzle2d/prototypes/minislotmachine/TestMiniSlotMachineLevelPrototypeScenario1ReelTileCollisions.java
+++ source/java/2d/core/src/test/java/com/ellzone/slotpuzzle2d/prototypes/minislotmachine/TestMiniSlotMachineLevelPrototypeScenario1ReelTileCollisions.java
@@ -1,7 +1,7 @@
 package com.ellzone.slotpuzzle2d.prototypes.minislotmachine;
 
 import com.ellzone.slotpuzzle2d.level.LevelCreatorScenario1;
-import com.ellzone.slotpuzzle2d.scene.Hud;
+import com.ellzone.slotpuzzle2d.puzzlegrid.PuzzleGridTypeReelTile;
 import com.ellzone.slotpuzzle2d.screens.PlayScreen;
 import com.ellzone.slotpuzzle2d.sprites.ReelTile;
 
@@ -11,32 +11,34 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.powermock.api.easymock.PowerMock;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.reflect.Whitebox;
 
-import static org.easymock.EasyMock.anyBoolean;
 import static org.easymock.EasyMock.capture;
 import static org.easymock.EasyMock.captureBoolean;
+import static org.easymock.EasyMock.captureInt;
 import static org.easymock.EasyMock.expect;
-import static org.easymock.EasyMock.replay;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.powermock.api.easymock.PowerMock.*;
 import static org.powermock.api.easymock.PowerMock.createMock;
-import static org.powermock.api.easymock.PowerMock.expectNew;
+import static org.powermock.api.easymock.PowerMock.mockStatic;
 import static org.powermock.api.easymock.PowerMock.verify;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest( {MiniSlotMachineLevelPrototypeScenario1.class} )
+@PrepareForTest( {MiniSlotMachineLevelPrototypeScenario1.class, PuzzleGridTypeReelTile.class} )
 
 public class TestMiniSlotMachineLevelPrototypeScenario1ReelTileCollisions {
     private static final String LEVEL_CREATOR_FIELD_NAME = "levelCreator";
+    private static final String GAME_LEVEL_HEIGHT_FIELD_NAME = "GAME_LEVEL_HEIGHT";
 
     private MiniSlotMachineLevelPrototypeScenario1 partialMockMiniSlotMachineLevelPrototypeScenario1;
     private ReelTile reelTileMock;
     private LevelCreatorScenario1 levelCreatorScenario1Mock;
     private Capture<Boolean> captureLevelCreatorSetHitSinkBottomArgument;
+    private Capture<ReelTile> captureSwapReelsAboveArgument;
+    private Capture<Integer> captureReelsLeftToFallArgumentRow, captureReelsLeftToFallArgumentCol;
 
     @Before
     public void setUp() {
@@ -46,8 +48,10 @@ public class TestMiniSlotMachineLevelPrototypeScenario1ReelTileCollisions {
     }
 
     private void setUpPowerMocks() {
-        partialMockMiniSlotMachineLevelPrototypeScenario1 = PowerMock.createNicePartialMock(MiniSlotMachineLevelPrototypeScenario1.class,
-                "handleInput");
+        partialMockMiniSlotMachineLevelPrototypeScenario1 = createNicePartialMock(MiniSlotMachineLevelPrototypeScenario1.class,
+                "swapReelsAboveMe",
+                "reelsLeftToFall");
+        mockStatic(PuzzleGridTypeReelTile.class);
     }
 
     private void setUpEasyMocks() {
@@ -57,6 +61,9 @@ public class TestMiniSlotMachineLevelPrototypeScenario1ReelTileCollisions {
 
     private void setUpCapture() {
         captureLevelCreatorSetHitSinkBottomArgument = EasyMock.newCapture();
+        captureSwapReelsAboveArgument = EasyMock.newCapture();
+        captureReelsLeftToFallArgumentRow = EasyMock.newCapture();
+        captureReelsLeftToFallArgumentCol = EasyMock.newCapture();
     }
 
     @After
@@ -65,12 +72,24 @@ public class TestMiniSlotMachineLevelPrototypeScenario1ReelTileCollisions {
     }
 
     @Test
-    public void testDealWithHitSinkBottom() {
+    public void testDealWithHitSinkBottomIntroSpinning() throws Exception {
         setFields();
-        setExpects();
+        setExpects(PlayScreen.PlayStates.INTRO_SPINNING);
         replayAll();
         partialMockMiniSlotMachineLevelPrototypeScenario1.dealWithHitSinkBottom(reelTileMock);
-        assertThat(captureLevelCreatorSetHitSinkBottomArgument.getValue(),is(true));
+        assertThat(captureLevelCreatorSetHitSinkBottomArgument.getValue(), is(true));
+        verifyAll();
+    }
+
+    @Test
+    public void testDealWithHitSinkBottomReelsFlashing() throws Exception {
+        setFields();
+        setExpects(PlayScreen.PlayStates.INTRO_FLASHING);
+        replayAll();
+        partialMockMiniSlotMachineLevelPrototypeScenario1.dealWithHitSinkBottom(reelTileMock);
+        assertThat(captureSwapReelsAboveArgument.getValue(), is(reelTileMock));
+        assertThat(captureReelsLeftToFallArgumentRow.getValue(), is(2));
+        assertThat(captureReelsLeftToFallArgumentCol.getValue(), is(2));
         verifyAll();
     }
 
@@ -78,16 +97,52 @@ public class TestMiniSlotMachineLevelPrototypeScenario1ReelTileCollisions {
         Whitebox.setInternalState(partialMockMiniSlotMachineLevelPrototypeScenario1, LEVEL_CREATOR_FIELD_NAME, levelCreatorScenario1Mock);
     }
 
-    private void setExpects() {
-        expect(levelCreatorScenario1Mock.getPlayState()).andReturn(PlayScreen.PlayStates.INTRO_SPINNING).atLeastOnce();
-        levelCreatorScenario1Mock.setHitSinkBottom(captureBoolean(captureLevelCreatorSetHitSinkBottomArgument));
+    private void setExpects(PlayScreen.PlayStates playState) throws Exception {
+        expect(levelCreatorScenario1Mock.getPlayState()).andReturn(playState);
+        if (playState == PlayScreen.PlayStates.INTRO_SPINNING) {
+            levelCreatorScenario1Mock.setHitSinkBottom(captureBoolean(captureLevelCreatorSetHitSinkBottomArgument));
+        }
+        expect(levelCreatorScenario1Mock.getPlayState()).andReturn(playState);
+        expect(levelCreatorScenario1Mock.getPlayState()).andReturn(playState);
+        expectIsFlashing(playState);
+    }
+
+    private void expectIsFlashing(PlayScreen.PlayStates playState) throws Exception {
+        if ((playState == PlayScreen.PlayStates.INTRO_FLASHING) |
+            (playState == PlayScreen.PlayStates.REELS_FLASHING))
+            expectFlashing();
+
+    }
+
+    private void expectFlashing() throws Exception {
+        expectrc();
+        expectSwapReelsAboveMe();
+    }
+
+    private void expectSwapReelsAboveMe() throws Exception {
+        expect(levelCreatorScenario1Mock.findReel(10, 120)).andReturn(0);
+        expect(reelTileMock.getDestinationX()).andReturn(10.0f);
+        expectPrivate(partialMockMiniSlotMachineLevelPrototypeScenario1, "swapReelsAboveMe", capture(captureSwapReelsAboveArgument));
+        expectPrivate(partialMockMiniSlotMachineLevelPrototypeScenario1, "reelsLeftToFall", captureInt(captureReelsLeftToFallArgumentRow), captureInt(captureReelsLeftToFallArgumentCol));
+    }
+
+    private void expectrc() {
+        expect(PuzzleGridTypeReelTile.getRowFromLevel(10.0f, 9)).andReturn(2);
+        expect(reelTileMock.getDestinationY()).andReturn(10.0f);
+        expect(PuzzleGridTypeReelTile.getColumnFromLevel(10.0f)).andReturn(2);
+        expect(reelTileMock.getDestinationX()).andReturn(10.0f);
     }
 
     private void replayAll() {
-        replay(reelTileMock, levelCreatorScenario1Mock);
+        replay(PuzzleGridTypeReelTile.class,
+               partialMockMiniSlotMachineLevelPrototypeScenario1,
+               reelTileMock,
+               levelCreatorScenario1Mock);
     }
 
     private void verifyAll() {
-        verify(reelTileMock, levelCreatorScenario1Mock);
+        verify(partialMockMiniSlotMachineLevelPrototypeScenario1,
+               reelTileMock,
+               levelCreatorScenario1Mock);
     }
 }
