785c5f68e9702e44b0cbc93e0e28afbe87745535
Mark Ellis <memellis@gmail.com>
2019-06-11 23:58:47 +0100

    ArtemisOdbQuickStart prototype

---
+++ source/java/2d/components/src/java/com/ellzone/slotpuzzle2d/component/Example.java
@@ -0,0 +1,7 @@
+package com.ellzone.slotpuzzle2d.prototypes.artemis.component;
+
+import com.artemis.Component;
+
+public class Example extends Component {
+    public float age;
+}
---
+++ source/java/2d/components/src/java/com/ellzone/slotpuzzle2d/component/Translation.java
@@ -0,0 +1,8 @@
+package com.ellzone.slotpuzzle2d.prototypes.artemis.component;
+
+import com.artemis.Component;
+
+public class Translation extends Component {
+    public float x;
+    public float y;
+}
