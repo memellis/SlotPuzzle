273d1947cb4d7982fac748301434ed5c0c6c8948
Mark Ellis <memellis@gmail.com>
2019-08-08 23:08:49 +0100

    Missing from commit when refeactored prototype directory structure which broke the build

--- source/java/2d/desktop/src/com/ellzone/slotpuzzle2d/desktop/MyPacker.java
+++ source/java/2d/desktop/src/com/ellzone/slotpuzzle2d/desktop/MyPacker.java
@@ -3,7 +3,7 @@ package com.ellzone.slotpuzzle2d.desktop;
 import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 public class MyPacker {
     public static void main (String[] args) throws Exception {
-        TexturePacker.process("../android/assets/tiles", "../android/assets/tiles", "tiles.pack");
+        TexturePacker.process("./desktop/assets/tiles", "./desktop/assets/tiles1", "tiles.pack");
     }
 }
 
