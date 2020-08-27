d6498b6198373bef8d81d247f01dee1afc529c2e
Mark Ellis <memellis@gmail.com>
2020-08-27 13:26:10 +0100

    Fixed ColorComponent and PositionComponent so compatible with Artemis

--- source/java/2d/components/src/java/com/ellzone/slotpuzzle2d/component/ColorComponent.java
+++ source/java/2d/components/src/java/com/ellzone/slotpuzzle2d/component/ColorComponent.java
@@ -4,8 +4,12 @@ import com.artemis.Component;
 import com.badlogic.gdx.graphics.Color;
 
 public class ColorComponent extends Component {
-    public Color color;
+    public float red, green, blue, alpha;
+    public ColorComponent() {}
     public ColorComponent(float red, float green, float blue, float alpha) {
-        this.color = new Color(red, green, blue, alpha);
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+        this.alpha = alpha;
     }
 }
--- source/java/2d/components/src/java/com/ellzone/slotpuzzle2d/component/PositionComponent.java
+++ source/java/2d/components/src/java/com/ellzone/slotpuzzle2d/component/PositionComponent.java
@@ -4,7 +4,7 @@ import com.artemis.Component;
 
 public class PositionComponent extends Component {
     public float x, y;
-
+    public PositionComponent() {}
     public PositionComponent (float x, float y) {
         this.x = x;
         this.y = y;
