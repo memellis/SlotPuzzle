a342fe4ebae82d560ebdccbc799b408a3ce89f4a
Mark Ellis <memellis@gmail.com>
2021-05-28 23:00:02 +0100

    Renamed SlotPuzzle to SlotPuzzleGame

--- source/java/2d/desktop/src/com/ellzone/slotpuzzle2d/desktop/DesktopLauncher.java
+++ source/java/2d/desktop/src/com/ellzone/slotpuzzle2d/desktop/DesktopLauncher.java
@@ -18,13 +18,13 @@ package com.ellzone.slotpuzzle2d.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.ellzone.slotpuzzle2d.SlotPuzzle;
+import com.ellzone.slotpuzzle2d.SlotPuzzleGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 1280;
 		config.height = 720;
-		new LwjglApplication(new SlotPuzzle(), config);
+		new LwjglApplication(new SlotPuzzleGame(), config);
 	}
 }
