9ce0d0f09befc8355e5b52b6fd70e2bb885943e5
Mark Ellis <memellis@gmail.com>
2021-05-29 14:44:20 +0100

    Now using renamed SlotPuzzleGame

--- source/java/2d/android/src/com/ellzone/slotpuzzle2d/android/AndroidLauncher.java
+++ source/java/2d/android/src/com/ellzone/slotpuzzle2d/android/AndroidLauncher.java
@@ -1,15 +1,16 @@
 package com.ellzone.slotpuzzle2d.android;
 
 import android.os.Bundle;
+
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.ellzone.slotpuzzle2d.SlotPuzzle;
+import com.ellzone.slotpuzzle2d.SlotPuzzleGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new SlotPuzzle(), config);
+		initialize(new SlotPuzzleGame(), config);
 	}
 }
