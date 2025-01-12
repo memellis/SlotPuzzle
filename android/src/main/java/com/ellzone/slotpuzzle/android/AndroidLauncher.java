0476bbb25bdc0e8d2ffcb9eda7cd58189debd48a
Mark Ellis <memellis@gmail.com>
2025-01-12 00:53:21 +0000

    - Fix starting SlotPuzzle on Android - Update version info

--- android/src/main/java/com/ellzone/slotpuzzle/android/AndroidLauncher.java
+++ android/src/main/java/com/ellzone/slotpuzzle/android/AndroidLauncher.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.ellzone.slotpuzzle.Main;
+import com.ellzone.slotpuzzle.SlotPuzzleGame;
 
 /** Launches the Android application. */
 public class AndroidLauncher extends AndroidApplication {
@@ -13,6 +13,6 @@ public class AndroidLauncher extends AndroidApplication {
         super.onCreate(savedInstanceState);
         AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
         configuration.useImmersiveMode = true; // Recommended, but not required.
-        initialize(new Main(), configuration);
+        initialize(new SlotPuzzleGame(), configuration);
     }
 }
